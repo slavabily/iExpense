6551095c0fd2ce6abb9d134a50da839b119eec5e
slavabily <slavabily@me.com>
2020-03-20 15:21:42 +0200

    Initial Commit

---
+++ iExpence/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  iExpence
+//
+//  Created by slava bily on 18/3/20.
+//  Copyright © 2020 slava bily. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExpenseItem: Identifiable, Codable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        self.items = []
+    }
+    
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var showingAddExpence = false
+    
+    @ObservedObject var expences = Expenses()
+ 
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expences.items) { (item) in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text("$\(item.amount)")
+                    }
+                    
+                }
+                .onDelete(perform: removeItems(at:))
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showingAddExpence = true
+                }, label: {
+                    Image(systemName: "plus")
+                }))
+        }
+        .sheet(isPresented: $showingAddExpence) {
+            AddView(expenses: self.expences)
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expences.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
